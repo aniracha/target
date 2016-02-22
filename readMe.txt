0256f51bb60b751c00fff12e234f76705bc474f5
Anirban Acharjee <anirbanacharjee@Anirbans-MacBook-Air.local>
2016-02-21 22:09:32 -0600

    added read me and performance test

---
+++ readMe.txt
@@ -0,0 +1,44 @@
+The code in this project searches for a pattern in a text file. Three types of search methods are used :
+ the naive way of searching a string within a text file; using regular expressions; using pre-processing hash map.
+ 
+ Let us investigate each approach :
+ 
+ 1. Naive approach :
+ 
+ I have used a naive string matching algorithm where the pattern string (the smaller string) is slid
+ over the larger String (which is the contents of the file). The worst case algorithmic time complexity for this
+ method is O(m*n), where m = length of the pattern, n = length of the contents of the file.
+ 
+ On doing a performance test over 2M search terms I got 6974.396 milli seconds.
+ 
+ The naive algorithm could be improved by using state of the art algorithms like KMP or Boyer Moore, which 
+ bring down the average search time to linear or even sublinear in the case of Boyer Moore.
+ 
+ 2. Regex approach :
+ 
+ The regex approach is even more time consuming than the naive method described above. The reason is in this method,
+ we need to go through all the substrings inside the file and check each substring with the pattern. For a text 
+ of length 'n' the number of substrings is n^2 and hence it takes O(n^2) time. 
+ 
+ The performance test took a very very long time.
+ 
+ 3. The preprocessing approach :
+ 
+ In this approach, I have created a hashmap for each of the substrings of the text. Granted that the preprocessing takes 
+ O(n^2) time, subsequent searches are done in O(1) time. 
+ 
+ There is a problem with this approach though. When I tried with large files, the code blew up giving a out of
+ memory exception (even after I threw more heap at it). This is however expected. For a string of length 'n', 
+ it has n^2 substrings and as the value of n increases, the hashmap explodes. So this preprocessing is only valid 
+ for smaller files.
+ 
+ I have tried the Trie data structure too for preprocessing larger files, but even that gave me a out of memory 
+ exception. The Trie would give a O(m) (m being the size of the search patter) access time as opposed to O(1) in a hashmap.
+ 
+ The performance test gave me a time of 2313.60 milli seconds for 2M searches. This is expectedly lesser than the 
+ naive approach as the strings already processed and their occurance counts stored in the hash map, so
+ retrieving them takes on O(1) time.  
+ 
+ 
+ 
+  
--- src/main/PatternChecker.java
+++ src/main/PatternChecker.java
@@ -3,11 +3,21 @@ package main;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class PatternChecker {
+
+    static StringBuffer str = new StringBuffer();
+    static List<String> newlist = new ArrayList<String>();
+   
+    static HashMap<String, HashMap<String, Integer>> fileNameToMap = new HashMap<String, HashMap<String, Integer>>();
     
     public static int findOccurancesNaive(String input, String pattern) {
         int lengthOfPattern = pattern.length();
@@ -28,26 +38,38 @@ public class PatternChecker {
         }
         return numberOfOccurances;
     }
-
-    public static int findIndexed(String input, String searchTerm) {
-    	HashMap<String, Integer> map = new HashMap<String, Integer>();
+    
+    private static void preprocessFile(String input, HashMap<String, Integer> map) {
+    	if (!map.isEmpty()) {
+    		return;
+    	}
         int lengthOfInput = input.length();
-
+        StringBuffer substring = new StringBuffer();
         for (int i = 0; i < lengthOfInput; i++) {
-            String substring = "";
+            
             for (int j = i; j < lengthOfInput; j++) {
-                substring += input.charAt(j);
-                //System.out.println(substring);
-
-                if (map.containsKey(substring)) {
-                    int value = map.get(substring);
-                    map.put(substring, ++value);
+            	substring.append(input.charAt(j));
+                if (map.containsKey(substring.toString())) {
+                    int value = map.get(substring.toString());
+                    map.put(substring.toString(), ++value);
                 } else {
-                    map.put(substring, 1);
+                    map.put(substring.toString(), 1);
                 }
             }
+            substring.setLength(0);
         }
+    }
 
+    public static int findIndexed(String file, String input, String searchTerm) {
+    	HashMap<String, Integer> map = null;
+    	if (fileNameToMap.get(file) == null) {
+    		map = new HashMap<String, Integer>();
+    		fileNameToMap.put(file, map);
+    		preprocessFile(input, map);
+    	} else {
+    		map = fileNameToMap.get(file);
+    	}
+    	
         if (map.get(searchTerm) != null) {
             return map.get(searchTerm);
         } else {
@@ -58,17 +80,23 @@ public class PatternChecker {
     public static int findOccurancesUsingRegex(String input, String regex) {
         int lengthOfInput = input.length();
         int numberOfOccurances = 0;
-
-        for (int i = 0; i < lengthOfInput; i++) {
-            String substring = "";
-            for (int j = i; j < lengthOfInput; j++) {
-                substring += input.charAt(j);
-                if (Pattern.matches(regex, substring)) {
-                    System.out.println(substring);
-                    numberOfOccurances++;
+        
+        try {
+        	Pattern p = Pattern.compile(regex);
+            StringBuffer substring = new StringBuffer();
+            for (int i = 0; i < lengthOfInput; i++) {
+            	
+                for (int j = i; j < lengthOfInput; j++) {
+                	substring.append(input.charAt(j));
+                	Matcher m = p.matcher(substring.toString());
+                    if (m.matches()) {
+                        numberOfOccurances++;
+                    }
                 }
-                
+                substring.setLength(0);
             }
+        } catch(PatternSyntaxException e) {
+        	return 0;
         }
 
         return numberOfOccurances;
@@ -76,7 +104,7 @@ public class PatternChecker {
 
     public static String readFile(String fileName) {
         BufferedReader br = null;
-        String str = "";
+        str.setLength(0);
 
         try {
 
@@ -85,7 +113,7 @@ public class PatternChecker {
                 br = new BufferedReader(new FileReader(fileName));
 
                 while ((sCurrentLine = br.readLine()) != null) {
-                    str += sCurrentLine;
+                    str.append(sCurrentLine);
                 }
 
             } catch (IOException e) {
@@ -97,10 +125,45 @@ public class PatternChecker {
                     ex.printStackTrace();
                 }
             }
-        return str;
+        String arr[] = str.toString().split(" ");
+        for (String s : arr) {
+        	newlist.add(s);
+        }
+        System.out.println("The length of array list is = " + newlist.size());
+                
+        
+        return str.toString();
+        
     }
+    
+    public static void doDiagnostic(String file) {
+    	Random random = new Random();
+    	int max = newlist.size()-1;
+    	int min = 0;
+    	long startTime = System.nanoTime();
+    	
+    	String input = readFile(file);
+    	for (long i = 0; i < 2000000; i++) {
+    		//int randomIndex = random.nextInt(max - min + 1) + min;
+    		int randomIndex = (int) (Math.random() * newlist.size());
+    		String pattern = newlist.get(randomIndex);
+    		//System.out.println(pattern);
+    		//int val = findOccurancesNaive(input, pattern);
+    		//int val = findIndexed(file, input, pattern);
+    		int val = findOccurancesUsingRegex(input, pattern);
+    		//System.out.println(val);
+    		
+    	}
+        long endTime = System.nanoTime();
+        System.out.println("That took " + (endTime - startTime) + " nanosecond");
+    	
+    }
+    
+    
 
     public static void trash() {
+    	
+    	
 
             // long startTime = System.nanoTime();;//System.currentTimeMillis();
 
@@ -131,17 +194,21 @@ public class PatternChecker {
             str = readFile(file);
 
             int numberOfOccurances = 0;
+            long startTime = System.nanoTime();
             switch (searchWay) {
                 case 1: numberOfOccurances = findOccurancesNaive(str, searchTerm);
                         break;
                 case 2: numberOfOccurances = findOccurancesUsingRegex(str, searchTerm);
                         break;
-                case 3: numberOfOccurances = findIndexed(str, searchTerm);
+                case 3: numberOfOccurances = findIndexed(file, str, searchTerm);
                         break;
                 default: System.out.println("Error inputing search option");
                         break;
 
             }
+            
+            long endTime = System.nanoTime();
+           // System.out.println("That took " + (endTime - startTime) + " nanosecond");
 
             System.out.println("         " + file.substring(file.lastIndexOf("/") + 1) + " - " + numberOfOccurances);
         }
@@ -156,7 +223,10 @@ public class PatternChecker {
         System.out.println("Search method: 1) String Match 2) Regular Expression 3) Indexed");
         reader = new Scanner(System.in);
         int searchWay = reader.nextInt();
-        String listOfFiles[] = {"/Users/anirbanacharjee/Downloads/sample_text/warp_drive.txt"};
+        String listOfFiles[] = {"/Users/anirbanacharjee/Downloads/sample_text_2/warp_drive.txt",
+                                 "/Users/anirbanacharjee/Downloads/sample_text_2/test1.txt",
+                                "/Users/anirbanacharjee/Downloads/sample_text_2/test2.txt"
+        		};
         doEvaluation(listOfFiles, searchTerm, searchWay);
 
     }
@@ -164,6 +234,7 @@ public class PatternChecker {
     public static void main(String[] args) {
 
         processInput();
+    	//doDiagnostic("/Users/anirbanacharjee/Downloads/sample_text_2/warp_drive.txt");
 
     }
 }
