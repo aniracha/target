c26b1ee9bba982afd6f51e76ccd724e5f66f0d55
Anirban Acharjee <anirbanacharjee@Anirbans-MacBook-Air.local>
2016-02-22 20:48:44 -0600

    add readme and java doc

--- readMe.txt
+++ readMe.txt
@@ -1,44 +1,59 @@
+This file contains information on what the algorithm in this project does and how to run it.
+
+The first section is about the algorithms and how they perform on the different datasets and their runtime complexities.
+
 The code in this project searches for a pattern in a text file. Three types of search methods are used :
  the naive way of searching a string within a text file; using regular expressions; using pre-processing hash map.
- 
+
  Let us investigate each approach :
- 
+
  1. Naive approach :
- 
+
  I have used a naive string matching algorithm where the pattern string (the smaller string) is slid
  over the larger String (which is the contents of the file). The worst case algorithmic time complexity for this
  method is O(m*n), where m = length of the pattern, n = length of the contents of the file.
- 
+
  On doing a performance test over 2M search terms I got 6974.396 milli seconds.
- 
- The naive algorithm could be improved by using state of the art algorithms like KMP or Boyer Moore, which 
- bring down the average search time to linear or even sublinear in the case of Boyer Moore.
- 
+
+ The naive algorithm could be improved by using state of the art algorithms like KMP or Boyer Moore, which
+ brings down the average search time to linear or even sublinear in the case of Boyer Moore.
+
  2. Regex approach :
- 
+
  The regex approach is even more time consuming than the naive method described above. The reason is in this method,
- we need to go through all the substrings inside the file and check each substring with the pattern. For a text 
- of length 'n' the number of substrings is n^2 and hence it takes O(n^2) time. 
- 
+ we need to go through all the substrings inside the file and check each substring with the pattern. For a text
+ of length 'n' the number of substrings is n^2 and hence it takes O(n^2) time.
+
  The performance test took a very very long time.
- 
- 3. The preprocessing approach :
- 
- In this approach, I have created a hashmap for each of the substrings of the text. Granted that the preprocessing takes 
- O(n^2) time, subsequent searches are done in O(1) time. 
- 
+
+ 3. The pre-processing approach :
+
+ In this approach, I have created a hashmap for each of the substrings of the text. Granted that the preprocessing takes
+ O(n^2) time, subsequent searches are done in O(1) time.
+
  There is a problem with this approach though. When I tried with large files, the code blew up giving a out of
- memory exception (even after I threw more heap at it). This is however expected. For a string of length 'n', 
- it has n^2 substrings and as the value of n increases, the hashmap explodes. So this preprocessing is only valid 
- for smaller files.
- 
- I have tried the Trie data structure too for preprocessing larger files, but even that gave me a out of memory 
- exception. The Trie would give a O(m) (m being the size of the search patter) access time as opposed to O(1) in a hashmap.
- 
- The performance test gave me a time of 2313.60 milli seconds for 2M searches. This is expectedly lesser than the 
- naive approach as the strings already processed and their occurance counts stored in the hash map, so
- retrieving them takes on O(1) time.  
- 
- 
- 
-  
+ memory exception (even after I threw more heap at it). This is however expected. For a string of length 'n',
+ it has n^2 substrings and as the value of n increases, the hashmap explodes. So this preprocessing is only valid
+ for smaller files. This method only works for the file "warp_drive" and doesn't work for the other two due to the large size of the files.
+
+ I have tried the Trie data structure too for preprocessing larger files, but even that gave me a out of memory
+ exception. The Trie would give a O(m)  (m being the size of the search patter) access time as opposed to O(1) in a hashmap.
+
+ The performance test gave me a time of 2313.60 milli seconds for 2M searches. This is expectedly lesser than the
+ naive approach as the strings have already been processed and their occurrences stored in the hash map, so
+ retrieving them takes on O(1) time.
+
+ Now coming to the problem of handing large content, we could use map reduce to solve this problem. We could also use distributed hash table.
+ If we have very large volume of requests coming in say 5000 requests/sec, one option would be to use a caching mechanism (LRU cache), where we cache the most frequently used words and their occurrences.
+
+How to run?
+
+Please checkout the project through github. The only file you’ll need is PatternChecker.java.
+Please mention the files you want to search (fully qualified paths) in the  "listOfFiles” array in the “ processInput” method and compile and run the program.
+
+javac PatternCheck.java
+java PatternCheck
+
+OR
+
+The green button in Eclipse.
--- src/main/PatternChecker.java
+++ src/main/PatternChecker.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,11 +13,26 @@ import java.util.regex.PatternSyntaxException;
 
 public class PatternChecker {
 
+	//Stringbuffer so that we are not creating String objects all the time.
     static StringBuffer str = new StringBuffer();
+    
+    //This id for diagnostics.
     static List<String> newlist = new ArrayList<String>();
    
+    // A map of file names to the occurrences of the various patterns in the file.
     static HashMap<String, HashMap<String, Integer>> fileNameToMap = new HashMap<String, HashMap<String, Integer>>();
     
+    
+    /*
+     * Finds the occurrences of the pattern in an input String.
+     * 
+     * @param input
+     * 			The input String
+     * @param searchTerm
+     * 			The pattern to be searched in the input String.
+     * @return The number of times the pattern occurs in the input String.
+     * 
+     * */
     public static int findOccurancesNaive(String input, String pattern) {
         int lengthOfPattern = pattern.length();
         int lengthOfInput = input.length();
@@ -39,6 +53,10 @@ public class PatternChecker {
         return numberOfOccurances;
     }
     
+    /*
+     * Private function to pre-process the String 
+     * 
+     * */
     private static void preprocessFile(String input, HashMap<String, Integer> map) {
     	if (!map.isEmpty()) {
     		return;
@@ -60,6 +78,19 @@ public class PatternChecker {
         }
     }
 
+    /*
+     * Finds the occurrences of the pattern in an input String. The file is also given as an input
+     * so that occurrences are stored per file. This uses hashmap to preprocess the String.
+     * 
+     * @param file
+     *         The name of the file which comprises of the input String
+     * @param input
+     * 			The input String
+     * @param searchTerm
+     * 			The pattern to be searched in the input String.
+     * @return The number of times the pattern occurs in the input String.
+     * 
+     * */
     public static int findIndexed(String file, String input, String searchTerm) {
     	HashMap<String, Integer> map = null;
     	if (fileNameToMap.get(file) == null) {
@@ -77,6 +108,17 @@ public class PatternChecker {
         }
     }
 
+    
+    /*
+     * Finds the occurrences of the regular expression pattern in an input String
+     * 
+     * @param input
+     * 			The input String
+     * @param regex
+     * 			The regular expression to be searched in the input String.
+     * @return The number of times the regex occurs in the input String.
+     * 
+     * */
     public static int findOccurancesUsingRegex(String input, String regex) {
         int lengthOfInput = input.length();
         int numberOfOccurances = 0;
@@ -102,18 +144,26 @@ public class PatternChecker {
         return numberOfOccurances;
     }
 
+    /*
+     * Reads the contents of a file and returns the contents.
+     * 
+     * @param fileName
+     * 			The name of the file.
+     * @return The contents of the file as a String
+     * 
+     * */
     public static String readFile(String fileName) {
         BufferedReader br = null;
         str.setLength(0);
 
         try {
 
-                String sCurrentLine;
+                String currentLIne;
                     
                 br = new BufferedReader(new FileReader(fileName));
 
-                while ((sCurrentLine = br.readLine()) != null) {
-                    str.append(sCurrentLine);
+                while ((currentLIne = br.readLine()) != null) {
+                    str.append(currentLIne);
                 }
 
             } catch (IOException e) {
@@ -125,33 +175,38 @@ public class PatternChecker {
                     ex.printStackTrace();
                 }
             }
+        
+        //This is used for diagnostic purposes.
         String arr[] = str.toString().split(" ");
         for (String s : arr) {
         	newlist.add(s);
         }
-        System.out.println("The length of array list is = " + newlist.size());
                 
         
         return str.toString();
         
     }
     
+    /*
+     * Calculates the time it takes to run a 2M query on the various search methods. 
+     * 
+     * @param file
+     * 			A String denoting which file to search.
+     * @param searchTerm
+     * 
+     * */
     public static void doDiagnostic(String file) {
-    	Random random = new Random();
-    	int max = newlist.size()-1;
-    	int min = 0;
     	long startTime = System.nanoTime();
     	
     	String input = readFile(file);
     	for (long i = 0; i < 2000000; i++) {
-    		//int randomIndex = random.nextInt(max - min + 1) + min;
+    		
     		int randomIndex = (int) (Math.random() * newlist.size());
     		String pattern = newlist.get(randomIndex);
-    		//System.out.println(pattern);
-    		//int val = findOccurancesNaive(input, pattern);
-    		//int val = findIndexed(file, input, pattern);
-    		int val = findOccurancesUsingRegex(input, pattern);
-    		//System.out.println(val);
+    		
+    		//findOccurancesNaive(input, pattern);
+    		//findIndexed(file, input, pattern);
+    		findOccurancesUsingRegex(input, pattern);
     		
     	}
         long endTime = System.nanoTime();
@@ -161,40 +216,29 @@ public class PatternChecker {
     
     
 
-    public static void trash() {
-    	
-    	
-
-            // long startTime = System.nanoTime();;//System.currentTimeMillis();
-
-            // System.out.println("The number of occurrences are for naive: " + findOccurancesNaive(str, "France"));
-
-            // long endTime = System.nanoTime();//System.currentTimeMillis();
-
-            // System.out.println("That took " + (endTime - startTime) + " nanosecond");
-
-            // preprocessString(str);
-
-            // System.out.println("After pre preprocessString");
-
-            // startTime = System.nanoTime();//System.currentTimeMillis();
-
-            // System.out.println("The number of occurrences are for indexed: " + map.get("France"));
-
-            // endTime = System.nanoTime();//System.currentTimeMillis();
-            // System.out.println("That took " + (endTime - startTime) + " nanosecond");
-    }
-
+    /*
+     * Given a list of files, a search term and the search way/method, finds the occurrences
+     * of the search term in the files.
+     * 
+     * @param listOfFiles
+     * 			An array of Strings containing the list of files.
+     * @param searchTerm
+     * 			The search String
+     * @param searchWay 
+     * 			An integer denoting which method should be used to find the pattern.
+     * 
+     * */
     public static void doEvaluation(String listOfFiles[], String searchTerm, int searchWay) {
         System.out.println();
         System.out.println("Search results:");
+        long startTime = System.nanoTime();
         for (String file : listOfFiles) {
             String str = "";
 
             str = readFile(file);
 
             int numberOfOccurances = 0;
-            long startTime = System.nanoTime();
+           
             switch (searchWay) {
                 case 1: numberOfOccurances = findOccurancesNaive(str, searchTerm);
                         break;
@@ -206,15 +250,20 @@ public class PatternChecker {
                         break;
 
             }
-            
-            long endTime = System.nanoTime();
-           // System.out.println("That took " + (endTime - startTime) + " nanosecond");
 
-            System.out.println("         " + file.substring(file.lastIndexOf("/") + 1) + " - " + numberOfOccurances);
+            System.out.println("         " + file.substring(file.lastIndexOf("/") + 1) + " - " + numberOfOccurances + " matches");
         }
+        long endTime = System.nanoTime();
+        System.out.println();
+        System.out.println("Elapsed time " + (endTime - startTime)/1000000 + " millisecond");
 
     }
 
+    /*
+     * Entry point of the program. Called from the main method.
+     * Processes the input files and finds the presence of the pattern in the search files.
+     * 
+     * */
     public static void processInput() {
         System.out.println("Enter the search term:");
         Scanner reader = new Scanner(System.in);
@@ -223,10 +272,11 @@ public class PatternChecker {
         System.out.println("Search method: 1) String Match 2) Regular Expression 3) Indexed");
         reader = new Scanner(System.in);
         int searchWay = reader.nextInt();
+        
         String listOfFiles[] = {"/Users/anirbanacharjee/Downloads/sample_text_2/warp_drive.txt",
                                  "/Users/anirbanacharjee/Downloads/sample_text_2/test1.txt",
                                 "/Users/anirbanacharjee/Downloads/sample_text_2/test2.txt"
-        		};
+        						};
         doEvaluation(listOfFiles, searchTerm, searchWay);
 
     }
